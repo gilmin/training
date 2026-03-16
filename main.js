74098ae1a0cb2a37ba26bc7fae054b0ee4e3331c
rlfalstjd1 <rlfalstjd1@naver.com>
2026-03-16 06:34:53 +0000

    Upload project to training repository

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// Add JS here
+document.getElementById('generate-btn').addEventListener('click', () => {
+    const numbersContainer = document.getElementById('numbers-container');
+    numbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+    for (const number of Array.from(numbers).sort((a, b) => a - b)) {
+        const numberElement = document.createElement('div');
+        numberElement.classList.add('number');
+        numberElement.textContent = number;
+        numbersContainer.appendChild(numberElement);
+    }
+});
